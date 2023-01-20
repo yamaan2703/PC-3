f735aafbc5421c92182b4545b3e1c0a4e5d3c10d
yamaan2703 <115937602+yamaan2703@users.noreply.github.com>
2023-01-21 00:34:11 +0500

    changes updated

--- pages/main.js
+++ pages/main.js
@@ -53,3 +53,21 @@ anime.timeline({loop: true})
     delay: 1000
   });
 /********************* About-Section Navbar End **********************/
+
+// Tab
+const tabs = document.querySelectorAll('[data-tab-target]')
+const tabContents = document.querySelectorAll('[data-tab-content]')
+
+tabs.forEach(tab => {
+  tab.addEventListener('click', () => {
+    const target = document.querySelector(tab.dataset.tabTarget)
+    tabContents.forEach(tabContent => {
+      tabContent.classList.remove('active')
+    })
+    tabs.forEach(tab => {
+      tab.classList.remove('active')
+    })
+    tab.classList.add('active')
+    target.classList.add('active')
+  })
+})
